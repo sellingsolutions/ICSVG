6618a1e0f76636c2b358fb60648344708dd8ec1e
Alexander Selling <alexander@icontrolapp.se>
2018-12-20 15:45:27 +0100

    Initial Commit

---
+++ ICSVG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ICSVG
+//
+//  Created by Alexander Selling on 2018-12-20.
+//  Copyright © 2018 Alexander Selling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
